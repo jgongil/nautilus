336fb42660606f59d9504e89a85f7bf8d5fe1410
Javier <javier.gonzalez.gil@es.ey.com>
2019-07-24 21:25:27 +0200

    first commit with updated boilerplate

---
+++ project-6/truffle.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "<Infura PROJECT ID>";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret.txt").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    },
+
+  }
+};
